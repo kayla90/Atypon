2ee39afa0e430b0ef90ff389892445064821ee0f
Shiming <Ren>
2014-10-31 11:52:50 -0400

    machine pattern detection script

---
+++ pyton scripts/machine_pattern.py
@@ -0,0 +1,35 @@
+__author__ = 'renshiming'
+
+import json
+f = open('/Users/renshiming/Desktop/capstone/log_split.json')
+lines = f.readlines()
+f.close()
+print(len(lines))
+
+dict = {}
+for line in lines:
+    try:
+        d = json.loads(line)
+        if 'sessionid' in d:
+            print d['sesionid']
+            if 'content' in dict:
+                content = json.loads(d['content'])
+                if 'articleid' in content:
+                    print content['articleid']
+                    if d['sessionid'] in dict:
+                        arr = dict[d['sessionid']]
+                        arr.append(content['articleid'])
+                    else:
+                        arr = []
+                        arr.append(content['articleid'])
+                        dict[d['sessionid']] = arr
+    except:
+        continue
+
+for session in dict:
+    arr = dict[session]
+    l = len(arr)
+    arr.sort(arr)
+    difference = int(arr[-1]) - int(arr[0])
+    if difference <= l:
+        print session
